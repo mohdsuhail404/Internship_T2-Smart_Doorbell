e85508c2b2e0a8ae069ae0b91dff6a4871c213fc
mohdsuhail404 <sohail722005@gmail.com>
2026-06-24 01:36:08 +0530

    Implement pin setup and digital control logic
    Added pin mode setup and digital read/write logic to control output based on input.

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,19 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
-
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  pinMode(8,INPUT);
+  pinMode(9,OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  if(digitalRead(8) == 0)
+  {
+    digitalWrite(9,HIGH);
+  }
+  else
+  {
+    digitalWrite(9,LOW);
+  }
 }
